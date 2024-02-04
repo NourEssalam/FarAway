46657513d5ec562430f04cea81b9b565a7df6a0b
NourEssalam <werteni11thug@gmail.com>
2024-02-04 21:16:11 +0100

    Lift up the Items state so we can use it in the siblings componenets"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./index.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false,
+  },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   useEffect(() => {
     document.title = "Your New Page Title";
   }, []);
+
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(itemObj) {
+    // We Can't use push method because it will mutate the state which is not allowed in React
+    setItems((items) => [...items, itemObj]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,16 +37,70 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    // add the the new item onject to the items array
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
-      <h3>What do you need for your trip</h3>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip ?</h3>
+
+      <select
+        name="id"
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+          <option value={i} key={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        name="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-function PackingList() {
-  return <div className="list">list</div>;
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
